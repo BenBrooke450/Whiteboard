5bbbf5a408fef38c0e69e4b294d771b8394004a7
BenBrooke450 <bensir@mail.com>
2024-12-14 16:57:47 +0000

    Basic

--- To Review.py
+++ To Review.py
@@ -684,3 +684,127 @@ print(canPlaceFlowers([0,0,0],2))
 #True
 
 
+
+
+
+
+
+
+
+
+
+
+#####################################################################################
+#                         682. Baseball Game
+#####################################################################################
+
+
+"""
+
+You are keeping the scores for a baseball game with strange rules.
+ At the beginning of the game, you start with an empty record.
+
+You are given a list of strings operations, where operations[i] is
+ the ith operation you must apply to the record and is one of the following:
+
+An integer x.
+Record a new score of x.
+
+'+'.
+Record a new score that is the sum of the previous two scores.
+
+'D'.
+Record a new score that is the double of the previous score.
+
+'C'.
+Invalidate the previous score, removing it from the record.
+
+Return the sum of all the scores on the record after applying all the operations.
+
+The test cases are generated such that the answer and all intermediate calculations fit in a 32-bit integer and that all operations are valid.
+
+
+
+Example 1:
+
+Input: ops = ["5","2","C","D","+"]
+Output: 30
+Explanation:
+"5" - Add 5 to the record, record is now [5].
+"2" - Add 2 to the record, record is now [5, 2].
+"C" - Invalidate and remove the previous score, record is now [5].
+"D" - Add 2 * 5 = 10 to the record, record is now [5, 10].
+"+" - Add 5 + 10 = 15 to the record, record is now [5, 10, 15].
+The total sum is 5 + 10 + 15 = 30.
+
+
+
+Example 2:
+
+Input: ops = ["5","-2","4","C","D","9","+","+"]
+Output: 27
+Explanation:
+"5" - Add 5 to the record, record is now [5].
+"-2" - Add -2 to the record, record is now [5, -2].
+"4" - Add 4 to the record, record is now [5, -2, 4].
+"C" - Invalidate and remove the previous score, record is now [5, -2].
+"D" - Add 2 * -2 = -4 to the record, record is now [5, -2, -4].
+"9" - Add 9 to the record, record is now [5, -2, -4, 9].
+"+" - Add -4 + 9 = 5 to the record, record is now [5, -2, -4, 9, 5].
+"+" - Add 9 + 5 = 14 to the record, record is now [5, -2, -4, 9, 5, 14].
+The total sum is 5 + -2 + -4 + 9 + 5 + 14 = 27.
+
+
+
+Example 3:
+
+Input: ops = ["1","C"]
+Output: 0
+Explanation:
+"1" - Add 1 to the record, record is now [1].
+"C" - Invalidate and remove the previous score, record is now [].
+Since the record is empty, the total sum is 0.
+
+
+"""
+
+def game(list1:list[int]):
+
+    list3 = []
+    n = 0
+    m = 0
+
+    while n < len(list1):
+
+        if list1[n] != "C" and list1[n] != "D" and list1[n] != "+":
+            list3.append(int(list1[n]))
+            n = n + 1
+            m = m + 1
+
+        elif list1[n] == "C":
+            list3.pop(-1)
+            n = n + 1
+            m = m - 1
+
+        elif list1[n] == "D":
+            list3.append((int(list3[m-1]))*2)
+            n = n + 1
+            m = m + 1
+
+        elif list1[n] == "+":
+            list3.append(int(list3[m - 1]) + int(list3[m - 2]))
+            n = n + 1
+            m = m + 1
+
+    return sum(list3)
+
+
+
+print(game(["5","-2","4","C","D","9","+","+"]))
+
+
+
+
+
+
+
