2ccaf3f5fca35581133760e530fb7bf61316f4b7
BenBrooke450 <bensir@mail.com>
2024-12-16 22:10:29 +0000

    Basic

--- To Review.py
+++ To Review.py
@@ -910,3 +910,75 @@ print(number([5, 4, 2, 3]))
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#####################################################################################
+#                         1528. Shuffle String
+#####################################################################################
+
+
+"""
+You are given a string s and an integer array indices of the same length.
+The string s will be shuffled such that the character at the ith position
+ moves to indices[i] in the shuffled string.
+
+Example 1:
+Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+Output: "leetcode"
+Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+
+
+Example 2:
+Input: s = "abc", indices = [0,1,2]
+Output: "abc"
+Explanation: After shuffling, each character remains in its position.
+
+
+
+"""
+
+def func(string:str , indices:list[int]):
+
+    abc = dict(zip(indices,[x for x in string]))
+    return "".join([value for key,value in sorted(abc.items())])
+
+
+
+
+
+print(func("codeleet", [4,5,6,7,0,2,1,3]))
+#leetcode
+
+
+
+
+
+
+
+
+
