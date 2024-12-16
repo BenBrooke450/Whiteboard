8340a7e6b55538cd647efa490ab54eac34a313ed
BenBrooke450 <bensir@mail.com>
2024-12-16 23:14:29 +0000

    Basic

--- To Review.py
+++ To Review.py
@@ -982,3 +982,88 @@ print(func("codeleet", [4,5,6,7,0,2,1,3]))
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#####################################################################################
+#           2535. Difference Between Element Sum and Digit Sum of an Array
+#####################################################################################
+
+
+
+"""
+
+You are given a positive integer array nums.
+
+The element sum is the sum of all the elements in nums.
+The digit sum is the sum of all the digits (not necessarily distinct) that appear in nums.
+Return the absolute difference between the element sum and digit sum of nums.
+
+Note that the absolute difference between two integers x and y is defined as |x - y|.
+
+
+
+Example 1:
+
+Input: nums = [1,15,6,3]
+Output: 9
+Explanation:
+The element sum of nums is 1 + 15 + 6 + 3 = 25.
+The digit sum of nums is 1 + 1 + 5 + 6 + 3 = 16.
+The absolute difference between the element sum and digit sum is |25 - 16| = 9.
+
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: 0
+Explanation:
+The element sum of nums is 1 + 2 + 3 + 4 = 10.
+The digit sum of nums is 1 + 2 + 3 + 4 = 10.
+The absolute difference between the element sum and digit sum is |10 - 10| = 0.
+
+"""
+
+
+def func(list1:list[int]):
+
+    split_numbers = [int(num2) for nums in list1 for num2 in str(nums)]
+    return abs(sum(split_numbers) - sum(list1))
+
+print(func([1,2,3,4]))
+#66
+
+
+
+
+
+
+
+
+
+
+
+
+
