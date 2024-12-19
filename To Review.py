d5002bd37388f5a45f4c77304c3b413da0198801
BenBrooke450 <bensir@mail.com>
2024-12-19 17:30:40 +0000

    Basic

--- To Review.py
+++ To Review.py
@@ -1179,3 +1179,68 @@ print(func([1,4,2,5,3]))
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#####################################################################################
+#                   2864. Maximum Odd Binary Number
+#####################################################################################
+
+
+
+"""
+
+You are given a binary string s that contains at least one '1'.
+
+You have to rearrange the bits in such a way that the resulting binary number is the maximum odd binary number that can be created from this combination.
+
+Return a string representing the maximum odd binary number that can be created from the given combination.
+
+Note that the resulting string can have leading zeros.
+
+
+
+Example 1:
+
+Input: s = "010"
+Output: "001"
+Explanation: Because there is just one '1', it must be in the last position. So the answer is "001".
+Example 2:
+
+Input: s = "0101"
+Output: "1001"
+Explanation: One of the '1's must be in the last position.
+    The maximum number that can be made with the remaining digits is "100". So the answer is "1001".
+
+"""
+
+
+
+def func_bin(string:str):
+
+    n = string.count("1")
+
+    return ((n - 1)*"1") + (len(string) - n)*"0" + "1"
+
+print(func_bin("0101"))
+#1001
+
+
+
+
