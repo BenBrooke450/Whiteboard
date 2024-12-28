ac730d61955b22646552b3ee1d45cb668051c210
BenBrooke450 <bensir@mail.com>
2024-12-28 15:41:06 +0000

    Basic

--- To Review.py
+++ To Review.py
@@ -1524,7 +1524,6 @@ Explanation: The digits are replaced as follows:
 - s[7] -> shift('d',4) = 'h'
 
 """
-from matplotlib.style.core import library
 
 x = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
      'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -1604,7 +1603,73 @@ def lib(s:str):
 
 
 
+#####################################################################################
+#          1534. Count Good Triplets
+#####################################################################################
 
+
+
+
+"""
+Given an array of integers arr, and three integers a, b and c.
+    You need to find the number of good triplets.
+
+A triplet (arr[i], arr[j], arr[k]) is good if the following conditions are true:
+
+0 <= i < j < k < arr.length
+|arr[i] - arr[j]| <= a
+|arr[j] - arr[k]| <= b
+|arr[i] - arr[k]| <= c
+Where |x| denotes the absolute value of x.
+
+Return the number of good triplets.
+
+
+
+Example 1:
+Input: arr = [3,0,1,1,9,7], a = 7, b = 2, c = 3
+Output: 4
+Explanation: There are 4 good triplets: [(3,0,1), (3,0,1), (3,1,1), (0,1,1)].
+
+
+Example 2:
+Input: arr = [1,1,2,2,3], a = 0, b = 0, c = 1
+Output: 0
+Explanation: No triplet satisfies all conditions.
+
+"""
+def func(arr: list[int], a: int, b: int, c: int):
+
+    f = 0
+    n = 0
+    m = 0
+    j = 0
+
+    for x in arr:
+
+        if f == len(arr) - 2:
+            break
+
+        else:
+            f = f + 1
+            n = 0
+
+        for y in arr[f + n:]:
+            n = n + 1
+            m = 0
+
+            for z in arr[f + n + m:]:
+                m = m + 1
+
+                print(x,y,z)
+
+                if ((abs(x - y) <= a) and (abs(y - z) <= b) and (abs(x - z) <= c)):
+                    print("PASSED: ",x,y,z)
+                    j = j + 1
+    return j
+
+print(func(arr = [5,5,2,6,4], a = 5, b = 4, c = 5))
+#10
 
 
 
