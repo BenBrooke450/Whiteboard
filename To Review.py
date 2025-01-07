d10d965d353960329b5473d6287db5e5172ed120
BenBrooke450 <bensir@mail.com>
2025-01-07 16:35:43 +0000

    Basic

--- To Review.py
+++ To Review.py
@@ -1963,7 +1963,50 @@ print(luckyNumbers([[3,7,8],[9,11,13],[15,16,17]]))
 
 
 
+#####################################################################################
+#          1436. Destination City
+#####################################################################################
 
+
+"""
+You are given the array paths, where paths[i] = [cityAi, cityBi]
+    means there exists a direct path going from cityAi to cityBi.
+    Return the destination city, that is, the city without any
+    path outgoing to another city.
+
+It is guaranteed that the graph of paths forms a line
+    without any loop, therefore, there will be exactly one destination city.
+
+
+
+Example 1:
+Input: paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+Output: "Sao Paulo"
+Explanation: Starting at "London" city you will reach "Sao Paulo"
+    city which is the destination city. Your
+    trip consist of: "London" -> "New York" -> "Lima" -> "Sao Paulo".
+
+Example 2:
+Input: paths = [["B","C"],["D","B"],["C","A"]]
+Output: "A"
+Explanation: All possible trips are:
+"D" -> "B" -> "C" -> "A".
+"B" -> "C" -> "A".
+"C" -> "A".
+"A".
+Clearly the destination city is "A".
+
+"""
+
+def destCity(paths: list[list[str]]) -> str:
+
+    for i,two in enumerate(paths):
+        if (two[1] not in [y for x in paths[:i] for y in x]) and (two[1] not in [y for x in paths[i+1:] for y in x]):
+            return two[1]
+
+
+print(destCity([["B","C"],["D","B"],["C","A"]]))
+#A
 
 
 
