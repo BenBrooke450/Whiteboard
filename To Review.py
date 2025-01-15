881ca5f980ee89632a30bc92184047cc3230a6cb
BenBrooke450 <bensir@mail.com>
2025-01-15 21:33:43 +0000

    Basic

--- To Review.py
+++ To Review.py
@@ -2099,6 +2099,76 @@ print(deleteGreatestValue([[10]]))
 
 
 
+#####################################################################################
+#          1374. Generate a String With Characters That Have Odd Counts
+#####################################################################################
+
+
+"""
+Given an integer n, return a string with n characters
+    such that each character in such string occurs an odd number of times.
+
+The returned string must contain only lowercase
+    English letters. If there are multiples valid strings, return any of them.
+
+
+
+Example 1:
+Input: n = 4
+Output: "pppz"
+Explanation: "pppz" is a valid string since the character 'p'
+    occurs three times and the character 'z' occurs once.
+    Note that there are many other valid strings such as "ohhh" and "love".
+
+Example 2:
+Input: n = 2
+Output: "xy"
+Explanation: "xy" is a valid string since the characters 'x'
+    and 'y' occur once. Note that there are many other valid strings such as "ag" and "ur".
+
+
+Example 3:
+Input: n = 7
+Output: "holasss"
+
+"""
+
+def generateTheString(n: int) -> str:
+    if n % 2 == 0:
+        return "h"*(n-1)+"b"
+    else:
+        return "h"*n
+
+
+##############################################
+
+def generateTheString(n: int) -> str:
+
+    return sum(n.count(x) for x in set(n) if n.count(x) % 2 !=0)
+
+print(generateTheString("pppz"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
