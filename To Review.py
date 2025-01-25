60624c350856ebb241226d999cc82cc22dac958c
BenBrooke450 <bensir@mail.com>
2025-01-25 18:50:48 +0100

    Basic

--- To Review.py
+++ To Review.py
@@ -2328,6 +2328,50 @@ print(maximumValue(["alic3","bob","3","4","00000"]))
 
 
 
+#####################################################################################
+#            2843. Count Symmetric Integers
+#####################################################################################
+
+
+"""
+You are given two positive integers low and high.
+
+An integer x consisting of 2 * n digits is symmetric if the sum
+    of the first n digits of x is equal to the sum of the last
+    n digits of x. Numbers with an odd number of digits are never symmetric.
+
+Return the number of symmetric integers in the range [low, high].
+
+
+
+Example 1:
+Input: low = 1, high = 100
+Output: 9
+Explanation: There are 9 symmetric integers between 1 and 100: 11, 22, 33, 44, 55, 66, 77, 88, and 99.
+
+Example 2:
+Input: low = 1200, high = 1230
+Output: 4
+Explanation: There are 4 symmetric integers between 1200 and 1230: 1203, 1212, 1221, and 1230.
+"""
+
+import numpy as np
+
+def countSymmetricIntegers(low: int, high: int) -> int:
+
+    number_array = np.arange(low,high+1).tolist()
+    even_len_array = [x for x in number_array if len(str(x)) % 2 == 0]
+
+    return sum(1 for x in even_len_array
+                if sum(int(t) for t in (str(x)[:int(len(str(x))/2)])) == sum(int(t) for t in (str(x)[int(len(str(x))/2):])))
+
+
+print(countSymmetricIntegers(low = 1, high = 1200))
+#14
+
+
+
+
 
 
 
