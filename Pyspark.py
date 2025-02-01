2674922813e359f0b7526e56a4d063ee5b8a7ec9
BenBrooke450 <bensir@mail.com>
2025-02-01 01:56:46 +0100

    Basic

--- Pyspark.py
+++ Pyspark.py
@@ -55,3 +55,47 @@ df.show()
 
 ###############################################################################
 
+
+#23 How to filter words that contain atleast 2 vowels from a series?
+
+"""
+df = spark.createDataFrame([('Apple',), ('Orange',), ('Plan',) , ('Python',) , ('Money',)], ['Word'])
+
+df.show()
++------+
+| Word|
++------+
+| Apple|
+|Orange|
+| Plan|
+|Python|
+| Money|
++------+
+
+"""
+
+# Initialize Spark session
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('Spark Playground').getOrCreate()
+
+from pyspark.sql.functions import to_date, dayofmonth, weekofyear, dayofyear, dayofweek, col, concat, lit
+
+df = spark.createDataFrame([('Apple',), ('Orange',), ('Plan',) , ('Python',) , ('Money',)], ['Word'])
+
+list1 = []
+vowls = ["a","e","i","o","u"]
+
+for n in df.collect():
+  t = 0
+  for v in n[0].lower():
+    if v in vowls:
+      t = t + 1
+      if t == 2:
+        break
+    elif len(n[0]) - 1 == t:
+      list1.append((n[0],))
+    else:
+      t = t + 1
+
+df = spark.createDataFrame(list1,["Letters_not_woth_two"]).show()
